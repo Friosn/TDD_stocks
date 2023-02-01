4a50b98f4d8c9d43d058c24e12b9cacf002a3387
Friosn <franz.rios.nino@gmail.com>
2023-02-01 14:55:04 +0100

    Problems with eslint-prettier configs WIP

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-const express = require('express')
-
-const app = express()
-
-const PORT = 8080
-
-app.listen(() => console.log(`Server listening on port ${PORT} 🚀🌕`))
+const express = require('express');
+const app = express();
+const PORT = 8080;
+app.listen(PORT, () => console.log(`Server listening on port ${PORT} 🚀🌕`));
+module.exports.app = app;
